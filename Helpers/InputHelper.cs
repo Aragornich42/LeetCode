0818781e9d92861ec7719afbbabf59cf326eff76
Vasiliy Kazhentsev <iitikbo0316kazhentsev@gmail.com>
2025-03-05 11:58:20 +0300

    Init + Problem 1

---
+++ Helpers/InputHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Helpers
+{
+    public static class InputHelper
+    {
+        public static int[] ParseToIntArray(string? input)
+        {
+            if(input is null)
+                throw new ArgumentNullException(nameof(input));
+
+            var inputs = input.Split(',');
+            var result = new int[inputs.Length];
+            int value;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (!int.TryParse(inputs[i], out value))
+                    throw new Exception($"Input string {inputs[i]} has non integer symbols");
+                result[i] = value;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,7 @@
+﻿using LeetCode.CustomProblems;
+using Helpers;
+
+var nums = Console.ReadLine();
+var target = Console.ReadLine()!;
+var result = new Problem1().TwoSum(InputHelper.ParseToIntArray(nums), int.Parse(target));
+Console.WriteLine(string.Join(',', result));
